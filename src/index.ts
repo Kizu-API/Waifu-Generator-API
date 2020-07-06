d20cfe11713c1bf47d8ec384e4b6b2acadb58458
grammer23 <satya.appkey@gmail.com>
2020-07-07 01:51:43 +0800

    :sparkles: Improve scrape data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello world")
+import WaifuGenerator from "./scraper/WaifuGenerator"
+import fs from "fs"
+import path from "path"
+import chalk from "chalk"
+const outputFile = path.join(__dirname, 'data/', 'data.json')
+
+WaifuGenerator()
+ .then((data):void => {
+    fs.writeFile(outputFile, JSON.stringify(data), err => {
+        if(err) {
+            console.log(err)
+        }
+        console.log(chalk.yellow.bgBlue(`\n Scraping data successfully finished. \n`))
+    })
+ }) 
