f9ee528571c4dfe0bf667d7be931f824ea955b2e
= <=>
2020-07-08 00:18:07 +0800

    :sparkles: Add server

---
+++ src/api/v1/index.ts
@@ -0,0 +1,26 @@
+import express, {Request, Response, json} from "express"
+import path from "path"
+import fs from "fs"
+
+const PORT = 3000
+const app:express.Application = express()
+const file:string = path.join(__dirname, '..', '..', 'data', 'data.json')
+let dataScrape:any
+fs.readFile(file, 'utf-8', async (err, data) => {
+    if(!err){
+        try{
+            dataScrape = await JSON.parse(data)
+        }catch(err) {
+            console.log('Error parsing JSON string:', err)
+        }
+    }
+})
+
+app.get("/api/v1", (req: Request, res: Response) => {
+    res.setHeader("Cache-Control", "public,max-age=3600,s-maxage=30")
+    res.json(dataScrape)
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
