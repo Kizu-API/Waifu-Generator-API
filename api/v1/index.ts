3df3b7e375e7ddecd055623a259f803ce29c3bd8
grammer23 <satya.appkey@gmail.com>
2020-07-09 16:56:59 +0800

    :truck: Move folder

--- api/v1/index.ts
+++ api/v1/index.ts
@@ -1,26 +1,24 @@
-import express, {Request, Response, json} from "express"
+import express, {Request, Response} from "express"
 import path from "path"
 import fs from "fs"
 
 const PORT = 3000
 const app:express.Application = express()
-const file:string = path.join(__dirname, '..', '..', 'data', 'data.json')
+const file:string = path.join(__dirname, '..', '..', 'src', 'data', 'data.json')
 let dataScrape:any
-let item:any
 fs.readFile(file, 'utf-8', async (err, data) => {
     if(!err){
         try{
             dataScrape = await JSON.parse(data)
-            item = dataScrape[Math.floor(Math.random() * dataScrape.length)]
-            console.log(item)
         }catch(err) {
             console.log('Error parsing JSON string:', err)
         }
     }
 })
 
-app.get("/api/v2", (req: Request, res: Response) => {
-    res.json(item)
+app.get("/api/v1", (req: Request, res: Response) => {
+    res.setHeader("Cache-Control", "public,max-age=3600,s-maxage=30")
+    res.json(dataScrape)
 })
 
 app.listen(PORT, () => {
